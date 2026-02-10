43c8c798cbe553f07f0d77d020674c8f9e4a7954
Ahmad <sheerazahmad2006@gmail.com>
2026-02-10 20:41:00 +0500

    Fix duplicate root route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserController;
 
+Route::get('/test', function () {
+    return 'Laravel is working';
+});
+
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
