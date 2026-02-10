915b62448bc228338e07af37105852d1191e12e7
Ahmad <sheerazahmad2006@gmail.com>
2026-02-10 20:43:08 +0500

    Bypass ListingController for homepage test

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserController;
 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/test', function () {
     return 'Laravel is working';
 });
 
 // All Listings
-Route::get('/', [ListingController::class, 'index']);
+// Route::get('/', [ListingController::class, 'index']);
 
 // Manage listings (MUST be before {listing})
 Route::get('/listings/manage', [ListingController::class, 'manage'])
